e5bdbbcca79ff271c8c8b20b9ae744f3591ffb30
Varun Sharma <varunsharma@MacBookPro.lan>
2026-05-31 14:32:18 +0530

    config: update Firebase configuration with production KampusGrid credentials

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,12 @@
 // 6. Paste your Firebase SDK config below:
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCyI9UBBSZbvbfKeyYp13-kCCZAVEw82WI",
+  authDomain: "kampusgrid.firebaseapp.com",
+  projectId: "kampusgrid",
+  storageBucket: "kampusgrid.firebasestorage.app",
+  messagingSenderId: "354413218045",
+  appId: "1:354413218045:web:00fbda04d37759859ed694"
 };
 
 // Auto-check if configuration has been customized
